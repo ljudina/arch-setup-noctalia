443e3567570403753a58b197b807b1bf40152545
Marko Jovanovic <marko@mikroe.com>
2026-06-13 07:00:01 +0200

    added profile daemon

--- scripts/40-services.sh
+++ scripts/40-services.sh
@@ -10,6 +10,9 @@ enable_service_now iwd.service
 # Bluetooth
 enable_service_now bluetooth.service
 
+# Power profile daemon (D-Bus activated, but enable for predictability)
+enable_service_now power-profiles-daemon.service
+
 # Docker: enable only to avoid install hangs on some machines
 enable_service docker.service
 warn "Docker enabled. Start after reboot or manually: sudo systemctl start docker"
